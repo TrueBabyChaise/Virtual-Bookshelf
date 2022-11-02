3923fc95b6a72a667a377461d2722d1e720c5795
Florent Lalise <florent.lalise@gmail.com>
2022-11-02 20:54:48 +0100

    add prop-types

--- client/src/components/AppContainer.jsx
+++ client/src/components/AppContainer.jsx
@@ -1,14 +1,20 @@
 import NavigationBar from "@components/Navigation/NavigationBar"
+import PropType from "prop-types"
 
 function AppContainer ({ children }) {
   return (
     <div className="h-full min-h-screen flex dark:bg-gray-900">
       <NavigationBar />
-      <div className="p-4">
+      <div className="flex-1 px-6 py-7">
+
         { children }
       </div>
     </div>
   )
+}
+
+AppContainer.propTypes = {
+  children: PropType.node.isRequired
 }
 
 export default AppContainer
