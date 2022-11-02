96dbabe102c953dc8cf918576b407a708b79eacb
Askou <tom.augier@epitech.eu>
2022-11-02 12:54:41 +0100

    remove print

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-console.log({ dbName: process.env.MONGO_DB, user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD})
 con = mongoose.connect(`mongodb://database:27017/`, { dbName: process.env.MONGO_DB, user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD, useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('Mongoose Connected'))
 .catch(e => console.log(e));
