5966ccb29862979834e50b7f68d84786dd96205a
Florent Lalise <florent.lalise@gmail.com>
2022-11-02 20:55:34 +0100

    change style

--- client/src/components/Navigation/NavigationTab.jsx
+++ client/src/components/Navigation/NavigationTab.jsx
@@ -1,11 +1,21 @@
-import { MdOutlineBook } from "react-icons/md"
+import PropType from "prop-types"
 
-function NavigationTab ({title}) {
+function NavigationTab ({title, Icon, className = '', selected = false}) {
+
+  const selectedStyle = 'bg-gray-800'
+
   return (
-    <a href="#" className="flex text-slate-50">
-      <MdOutlineBook className="w-5 h-5" /> { title }
+    <a href="#" className={`flex text-slate-100 py-3 px-3 my-1 hover:bg-gray-700 w-full rounded-lg ${className} ${selected ? selectedStyle : ''}`}>
+      <Icon className="w-6 h-6" /> <p className="ml-3 text-sm font-medium my-auto">{ title }</p>
     </a>
   )
+}
+
+NavigationTab.propTypes = {
+  title: PropType.string.isRequired,
+  Icon: PropType.elementType.isRequired,
+  className: PropType.string,
+  selected: PropType.bool
 }
 
 export default NavigationTab
