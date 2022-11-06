e1975a3a6b7cf582285ef8ebf8fa107b23c97672
Florent Lalise <florent.lalise@gmail.com>
2022-11-06 17:08:07 +0100

    update BookCard UI

--- client/src/ui/Cards/BookCard.jsx
+++ client/src/ui/Cards/BookCard.jsx
@@ -1,14 +1,15 @@
 import PropType from "prop-types"
 import Image from "next/image"
 
-function BookCard ({ cover, title }) {
+function BookCard ({ cover, title, clickAction, className = '' }) {
   return (
-    <div className="w-52 h-72 relative rounded-xl overflow-hidden group cursor-pointer">
+    <div className={`relative rounded-xl overflow-hidden group cursor-pointer ${className}`} 
+      onClick={clickAction}>
       <Image src={cover} 
         fill={true} 
         className="object-cover" 
         alt={`Cover of the book ${title}`} />
-      <div className="absolute bottom-0 px-2 py-2 bg-gray-900/70 hidden group-hover:block">
+      <div className="absolute bottom-0 px-2 py-2 bg-gray-900/70 hidden group-hover:block w-full">
         <p className="text-slate-100">{ title }</p>
       </div>
     </div>
@@ -18,6 +19,8 @@ function BookCard ({ cover, title }) {
 BookCard.propTypes = {
   cover: PropType.string.isRequired,
   title: PropType.string.isRequired,
+  clickAction: PropType.func,
+  className: PropType.string
 }
 
 export default BookCard
