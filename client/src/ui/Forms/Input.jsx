9f1fe8bd02dbbd1f56cfb853b4060bdcdc629042
Florent Lalise <florent.lalise@gmail.com>
2022-11-08 23:24:54 +0100

    Better input

--- client/src/ui/Forms/Input.jsx
+++ client/src/ui/Forms/Input.jsx
@@ -1,24 +1,28 @@
 import PropType from "prop-types"
 
-function Input ({ type, name, inputClass, className, Icon, label, placeholder = '' }) {
+function Input ({ type, name, inputClass, className, Icon, label, onChange, value, placeholder = '' }) {
   return (
-    <>
-      { label && <label htmlFor={name}>{label}</label> }
-      <div className={`bg-gray-700 rounded flex p-1 h-9 focus-within:outline focus-within:outline-gray-800 ${className}`}>
+    <div className={`${className}`}>
+      { label && <label htmlFor={name} className="text-slate-300">{label}</label> }
+      <div className={`bg-gray-700 rounded flex px-2 py-1 h-9 focus-within:outline focus-within:outline-gray-800`}>
         {Icon && <Icon className="ml-1 mr-2 my-auto text-slate-100 w-6 h-6" />}
         <input type={type}
           name={name}
           placeholder={placeholder}
           title={name} 
-          className={`bg-transparent text-slate-100 outline-0 text-sm h-full my-auto ${inputClass}`} />
+          onChange={onChange}
+          value={value}
+          className={`bg-transparent text-slate-100 outline-0 text-sm h-full my-auto w-full ${inputClass}`} />
       </div>
-    </>
+    </div>
   )
 }
 
 Input.propTypes = {
   type: PropType.string.isRequired,
   name: PropType.string.isRequired,
+  onChange: PropType.func,
+  value: PropType.any,
   inputClass: PropType.string,
   className: PropType.string,
   Icon: PropType.elementType,
