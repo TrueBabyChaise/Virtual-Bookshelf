78902bc1b40f4644989a233c091caf0c8a8b8892
Florent Lalise <florent.lalise@gmail.com>
2022-12-04 22:32:39 +0100

    add user menu to logout

--- client/src/components/Navigation/NavigationBar.jsx
+++ client/src/components/Navigation/NavigationBar.jsx
@@ -1,21 +1,26 @@
 import NavigationTab from "@components/Navigation/NavigationTab"
 import Search from "@components/Search/Search"
-import { useState } from "react"
+import { useState, Fragment } from "react"
 import { useAuth } from "@hooks/useAuth"
-import { MdOutlineHome, MdOutlineLibraryAdd, MdOutlineSettings, MdOutlineSupervisedUserCircle } from "react-icons/md"
+import { Menu, Transition } from "@headlessui/react"
+import { MdOutlineHome, MdOutlineLibraryAdd, MdOutlineSettings, MdOutlineSupervisedUserCircle, MdOutlineLogout } from "react-icons/md"
 
 function NavigationBar({}) {
   const [searchOpened, setSearchOpened] = useState(false)
-  const { user } = useAuth()
+  const { user, signout } = useAuth()
 
-  const handleAddBook = () => {
+  /**
+   * Toggle book search on page
+   */
+  const toggleSearch = () => {
+    document.querySelector('body').classList.toggle('overflow-hidden')
     setSearchOpened(prev => !prev)
   }
 
   return (
     <>
       <div className="flex w-full h-[80px] sticky top-0 border-b border-gray-700 bg-gray-900 z-10">
-        <div className="flex items-center overflow-hidden px-4 container mx-auto">
+        <div className="flex items-center px-4 container mx-auto">
           <div className="pr-5 border-r border-gray-700">
             <a href="#" className="flex items-center px-3 py-1 rounded bg-white">
               <p className="mx-auto text-xl font-bold text-gray-900">B</p>
@@ -29,22 +34,46 @@ function NavigationBar({}) {
                 selected={true}
                 className="" />
               <NavigationTab subtitle="Add a book" 
-                handleAction={handleAddBook}
+                onClick={toggleSearch}
                 Icon={MdOutlineLibraryAdd} />
               
             </div>
           </div>
 
+          {/* TODO: Faire un composant user menu */}
           <div className="ml-auto flex justify-center gap-2">
-            
-            <NavigationTab title={ user.username }
-              Icon={MdOutlineSupervisedUserCircle} />
-            <NavigationTab subtitle="Settings" 
-              Icon={MdOutlineSettings} />
+            <Menu as="div" className="relative inline-block">
+              <Menu.Button>
+                <NavigationTab title={ user.username }
+                  Icon={MdOutlineSupervisedUserCircle} />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95">
+                <Menu.Items className="absolute right-0 mt-2 w-56 rounded-md bg-gray-800 shadow-lg text-slate-300">
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      <button className="group flex w-full items-center rounded-md px-2 py-2 text-sm"
+                        onClick={signout}>
+                        <MdOutlineLogout className="mr-2 h-5 w-5" />
+                        Log out
+                      </button>
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+            {/* <NavigationTab subtitle="Settings" 
+              Icon={MdOutlineSettings} /> */}
           </div>
         </div>
       </div>
-      { searchOpened && <Search setOpened={setSearchOpened} /> }
+      { searchOpened && <Search toggleSearch={toggleSearch} /> }
     </>
   )
 }
