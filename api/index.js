e905e58ee8b3daaea5d73716989dea2b0e3c423a
Tom <tomaugier23@gmail.com>
2022-12-19 14:12:30 +0100

    change origin

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(
-        cors({origin: 'http:localhost:3000', credentials: true, origin: true})
+        cors({origin: 'http:164.92.237.184:3000', credentials: true, origin: true})
 );
 
 /*app.use(
