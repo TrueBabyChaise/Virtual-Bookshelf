c8034321acdc16e754c67e5458bbcfb7aa7d7aec
Florent Lalise <florent.lalise@gmail.com>
2023-01-28 18:46:34 +0100

    Text when no book was found or loading

--- client/src/components/Books/BookList.jsx
+++ client/src/components/Books/BookList.jsx
@@ -1,5 +1,6 @@
 import PropType from "prop-types"
 import BookCard from "@src/ui/Cards/BookCard"
+import { MdOutlineLibraryAdd } from "react-icons/md"
 
 function BookList ({ booksInput, loading }) {
   const handleClick = e => {
@@ -10,16 +11,28 @@ function BookList ({ booksInput, loading }) {
 
   return (
     // <div className="grid grid-rows-[repeat(auto-fill,18rem)] grid-cols-[repeat(auto-fill,13rem)] gap-5 justify-center">
-    <div className="grid gap-5 justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
+    <>
       {
         !loading && booksInput.length > 0 ? booksInput.map((e,i) => (
-          <BookCard cover={e.thumbnail}
-            className="h-64"
-            title={e.title}
-            key={i}
-            clickAction={handleClick} /> 
-        )) : !loading ? 'No books found' : "Loading..."
+          <div className="grid gap-5 justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
+            <BookCard cover={e.thumbnail}
+              className="h-64"
+              title={e.title}
+              key={i}
+              clickAction={handleClick} /> 
+          </div>
+        )) : !loading ? 
+        <BookListInfo>You don't have a book yet... Try adding one by clicking on <MdOutlineLibraryAdd className="inline-block" />.</BookListInfo> : 
+        "Loading..."
       }
+    </>
+  )
+}
+
+function BookListInfo ({ children }) {
+  return (
+    <div className="flex flex-col justify-center items-center py-8">
+      <p className="text-lg text-slate-100 font-light">{children}</p>
     </div>
   )
 }
