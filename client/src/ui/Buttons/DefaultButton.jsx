58d25e1b05c488b14bcb7eddff8ecff8f7d30173
Florent Lalise <florent.lalise@gmail.com>
2023-05-11 15:46:37 +0200

    Add other props on the btn

--- client/src/ui/Buttons/DefaultButton.jsx
+++ client/src/ui/Buttons/DefaultButton.jsx
@@ -1,11 +1,11 @@
 import PropType from "prop-types"
 
-function DefaultButton ({ title, clickAction, Icon, className, type }) {
+function DefaultButton ({ title, clickAction, Icon, className, type, ...props }) {
 
   return (
     <button onClick={clickAction}
       type={type}
-      className={`px-2 py-1 min-w-[5rem] rounded flex justify-center items-center bg-slate-100 text-gray-900 ${className}`}>
+      className={`px-2 py-1 min-w-[5rem] rounded flex justify-center items-center bg-slate-100 text-gray-900 ${className}`} {...props}>
         { Icon && <Icon className="mr-1" /> }
         { title }
       </button>
